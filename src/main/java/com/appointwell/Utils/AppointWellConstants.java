cd443bfae026907d7a59fd323fcd6053223efff5
Ashbir S. Dhiman <ashbirashu@gmail.com>
2025-02-02 12:18:27 -0500

    Adding Basic Constants for messaging

---
+++ src/main/java/com/appointwell/Utils/AppointWellConstants.java
@@ -0,0 +1,7 @@
+package com.appointwell.Utils;
+
+public class AppointWellConstants {
+
+    public static final String SOMETHING_WENT_WRONG="SOMETHING_WENT_WRONG";
+    public static final String INVALID_DATA="Invalid Data";
+}
